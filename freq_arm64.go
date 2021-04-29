2f6da49de4fa63537b3b439f23b3b776d54f022e
Didier SPEZIA <dspezia@amadeus.com>
2021-04-29 16:35:51 +0200

    Adjust number of cycles for ARM frequency measurement loop

--- freq_arm64.go
+++ freq_arm64.go
@@ -1,7 +1,7 @@
 package main
 
 // ASMLoopCycles is the number of cycles of 1 iteration of the CountASM loop
-const ASMLoopCycles = 1026.0
+const ASMLoopCycles = 1025.0
 
 // NFREQ is the number of iterations. Adjust if it too slow/too fast.
 const NFREQ = 1 << 34
